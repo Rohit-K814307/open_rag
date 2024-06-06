54e325dbdcafb6ee1d8f847f95340e2a8300e37e
rohit-k814307 <kulkarni.rohitva@gmail.com>
2024-06-05 23:17:05 -0400

    pip reqs included

--- open_rag_api/api_setup.sh
+++ open_rag_api/api_setup.sh
@@ -1,14 +1,8 @@
 curl -fsSL https://ollama.com/install.sh | sh
 
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
-~/miniconda3/bin/conda init zsh
-
+pip install -r requirements.txt
 
 git clone https://github.com/Rohit-K814307/open_rag/tree/windows
 cd open_rag
 conda env create -f environment.yml
-ollama create email_model_llama2 -f ./email_model_llama2.txt 
+#ollama create email_model_llama2 -f ./email_model_llama2.txt 
