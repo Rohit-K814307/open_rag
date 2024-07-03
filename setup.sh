159376f3531d81742b2f4b17143a5844049953f1
rohit-k814307 <kulkarni.rohitva@gmail.com>
2024-07-03 00:06:35 -0400

    app updated

--- setup.sh
+++ setup.sh
@@ -1,3 +1,1 @@
-pip install -r requirements.txt
-curl -fsSL https://ollama.com/install.sh | sh
-ollama serve & ( ollama create email_model_llama2 -f email_model_llama2.txt && python3 -m open_rag_api.app)
+pip install -r requirements.txt && (curl -fsSL https://ollama.com/install.sh | sh && (ollama serve & ( ollama create email_model_llama2 -f email_model_llama2.txt && python3 -m open_rag_api.app)))
