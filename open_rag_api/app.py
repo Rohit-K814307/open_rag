61ca20deebc97a2a69a407c54a48784ddf143922
rohit-k814307 <kulkarni.rohitva@gmail.com>
2024-07-03 19:33:20 -0400

    changed to ngrok2

--- open_rag_api/app.py
+++ open_rag_api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import pandas as pd
 from open_rag.utils.generation import generate_email
-from flask_ngrok import run_with_ngrok
+from flask_ngrok2 import run_with_ngrok
 
 app = Flask(__name__)
 run_with_ngrok(app)
