9a090b817ffe3a1c7b9f5611bf1b41a77c5db0d7
rohit-k814307 <kulkarni.rohitva@gmail.com>
2024-07-02 23:15:56 -0400

    changed

--- setup.sh
+++ setup.sh
@@ -1,5 +1,1 @@
-pip install -r requirements.txt
-
-curl -fsSL https://ollama.com/install.sh | sh
-
-ollama serve & ollama create email_model_llama2 -f email_model_llama2.txt && python3 -m open_rag_api.app
+ollama serve & curl -fsSL https://ollama.com/install.sh | sh && pip install -r requirements.txt && ollama create email_model_llama2 -f email_model_llama2.txt && python3 -m open_rag_api.app
